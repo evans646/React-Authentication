0e011cb8564f7f30ad31c83ae005ff223741009a
Evans <jw207740@gmail.com>
2021-08-18 12:42:44 +0000

    changes made to the project

--- src/routes/signUpRoute.js
+++ src/routes/signUpRoute.js
@@ -42,7 +42,7 @@ export const signUpRoute = {
                     to: email,
                     from: 'jw207740@gmail.com',
                     subject: 'Email Verification',
-                    text: `Thanks for signing up, click here to verify your email address : http://localhost:${process.env.PORT||8080}/verify-email/${verificationString}`,
+                    text: `Thanks for signing up, click here to verify your email address : http://${process.env.PORT||8080}/verify-email/${verificationString}`,
                 });
             } catch (e) {
                 console.log(e)
