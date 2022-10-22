bf1a7c431ca8cd30711084173ea6dae5f80e6ec0
Evans Ansong <65941289+evans646@users.noreply.github.com>
2022-10-22 02:54:15 -0400

    feat:  removed unneeded files

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017';
 
 let client;
 
